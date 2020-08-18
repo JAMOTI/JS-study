83a0370f6de6cb56fb8cf62c2c0b7bf5bf2e9c11
Kseyeon <64938612+Kseyeon@users.noreply.github.com>
2020-08-19 07:44:10 +0900

    Add files via upload

---
+++ nomadcoders/#2-5/index.js
@@ -0,0 +1,9 @@
+const age = prompt("How old are you");
+
+if (age >= 18 && <= 21){
+    console.log("you can drink but you shoul not");
+}else if (age > 21){
+    console.log("go ahed");
+}else{
+    console.log("too young");
+}
